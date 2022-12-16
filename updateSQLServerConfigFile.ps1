08d1edf530562ac45bf9a8976d0c293e8eaa17b2
martensens <95280931+martensens@users.noreply.github.com>
2022-12-16 16:49:32 +0100

    Update updateSQLServerConfigFile.ps1

--- updateSQLServerConfigFile.ps1
+++ updateSQLServerConfigFile.ps1
@@ -1,11 +1,11 @@
-﻿$data = Import-Csv "C:\Users\Administrator\Desktop\config.csv"
+﻿$data = Import-Csv "C:\Temp\*config.csv"
 foreach($info in $data){
-    $info.Beschreibung | Out-File -FilePath "C:\Users\Administrator\Desktop\cfg.ini" -Append
+    $info.Beschreibung | Out-File -FilePath "C:\Temp\cfg.ini" -Append
     if($info.Individual.ToString() -ne ''){
       $eingabe = $info.Type + '"' + $info.Individual + '"'
-      $eingabe | Out-File -FilePath "C:\Users\Administrator\Desktop\cfg.ini" -Append 
+      $eingabe | Out-File -FilePath "C:\Temp\cfg.ini" -Append 
       }
     else{
-      $info.Default | Out-File -FilePath "C:\Users\Administrator\Desktop\cfg.ini" -Append
+      $info.Default | Out-File -FilePath "C:\Temp\cfg.ini" -Append
       }
     }
