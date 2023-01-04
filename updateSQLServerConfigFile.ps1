438dcd3fa1f3798e8d88ebc42056c6cc8cc77322
martensens <stiller.martin@freenet.de>
2023-01-04 11:26:16 +0100

    Anpassungen des Pfades

--- updateSQLServerConfigFile.ps1
+++ updateSQLServerConfigFile.ps1
@@ -1,11 +1,13 @@
-﻿$data = Import-Csv "C:\Temp\*config.csv"
+﻿#Anpassen der cfg.ini an die Vorgaben
+$Pfad = "C:\Temp\cfg.ini"
+$data = Import-Csv $Pfad
 foreach($info in $data){
-    $info.Beschreibung | Add-Content -Path "C:\Temp\cfg.ini"
+    $info.Beschreibung | Add-Content -Path $Pfad
     if($info.Individual.ToString() -ne ''){
       $eingabe = $info.Type + '"' + $info.Individual + '"'
-      $eingabe | Add-Content -Path "C:\Temp\cfg.ini"
+      $eingabe | Add-Content -Path $Pfad
       }
     else{
-      $info.Default | Add-Content -Path "C:\Temp\cfg.ini"
+      $info.Default | Add-Content -Path $Pfad
       }
     }
